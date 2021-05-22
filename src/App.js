3960cdfa72dada846827e0a132c10bbfbfe3a387
Cozack <dudycholeg@gmail.com>
2021-05-22 12:37:49 +0300

    1st part of redux class work is ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+
+const Posts = () => {
+    const {isLoading, posts} = useSelector(({isLoading, posts}) => ({isLoading, posts}));
+    const dispatch = useDispatch();
+    const fetchPosts = async () => {
+        try {
+            dispatch({type:'SET_IS_LOADING'});
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const payload = await response.json();
+            dispatch({type: 'SET_POSTS', payload});
+        } catch (e) {
+            console.log(e, 'aallala')
+        }
+        dispatch({type:'RESET_IS_LOADING'});
+    }
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+    if (isLoading){
+        return (
+            <h1>Loading</h1>
+        )
+    }
+
+    return (
+        <div>
+        {posts.map((post) => (
+            <p key={post.id}>{post.title} - {post.body}</p>
+        ))}
+    </div>
+    );
+
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Posts/>
+        </div>
+    );
 }
 
 export default App;
